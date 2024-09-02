dc11f92122ac1f11422817c7235b0a1d00f23a30
shaikh nikkhat <shaikhnikkhatparvin24@gmail.com>
2024-09-02 14:03:56 +0530

    update records

---
+++ src/entity/UpdateRecord.java
@@ -0,0 +1,31 @@
+package entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateRecord {
+    public static void main(String[] args) {
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+            Statement statement = connection.createStatement();
+            int affectedRows = statement.executeUpdate("update emp set sal=90000, name='tejas' where id =10 ; ");
+
+            if (affectedRows>0){
+                System.out.println("update record success");
+            }else {
+                System.out.println("update record failed");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
